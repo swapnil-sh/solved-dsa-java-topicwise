bb8b68cbe39850882f593776dd45566810322e8a
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-09-23 16:45:20 +0530

    1351. Count Negative Numbers in a Sorted Matrix - Leetcode Binary Search Java Solution

--- BinarySearch/CountNeginMatrix.java
+++ BinarySearch/CountNeginMatrix.java
@@ -1,4 +1,4 @@
-//1351. Count Negative Numbers in a Sorted Matrix - Binary Search Java Solution
+//1351. Count Negative Numbers in a Sorted Matrix - Leetcode Binary Search Java Solution
 
 class CountNeginMatrix 
 {
@@ -21,29 +21,10 @@ class CountNeginMatrix
         return pos;
     }
     
-    public static int findLastOccur(int a[],int st,int end)
-    {
-        int pos=-1;
-        while(st<=end)
-        {
-            int mid=st+(end-st)/2;
-            if(a[mid]<0)
-            {
-                pos=mid;
-                st=mid+1;
-            }
-            else if(a[mid]>=0)
-                st=mid+1;
-      
-        }
-        
-        return pos;  
-        
-    } 
     
     public int countNegatives(int[][] grid)
     {
-        int i,j,pos1=0,pos2=0,ans=0;
+        int i,j,pos1=0,pos2=-1,ans=0;
         int a[]=new int[grid[0].length];
         
         for(i=0;i<grid.length;i++)
@@ -54,9 +35,9 @@ class CountNeginMatrix
             }
             
             pos1=findFirstOccur(a,0,grid[i].length-1);
-            pos2=findLastOccur(a,0,grid[i].length-1);
-            System.out.println(pos1+" "+pos2);
-            if(pos1==-1 && pos2==-1)
+            pos2=grid[i].length-1;
+            //System.out.println(pos1+" "+pos2);
+            if(pos1==-1)
                 ans+=0;
             else
                 ans=ans+Math.abs(pos1-pos2)+1;
@@ -66,4 +47,5 @@ class CountNeginMatrix
 
         
     }
+
 }
