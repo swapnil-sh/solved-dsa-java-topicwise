89542ca0343aefb8c4c042532ea3c9ad18c40723
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-03 01:02:17 +0530

    Added Delete,Length and Search funtions

--- LinkedLists/LinkedList.java
+++ LinkedLists/LinkedList.java
@@ -15,7 +15,7 @@ class Linked
 		temp.next=null;
 		return temp;
 	}
-
+	
 	public Node insert(int x,int pos,Node head)
 	{
 		if(pos==1)
@@ -44,6 +44,49 @@ class Linked
 		return head;
 	}
 	
+	public Node delete(Node head,int pos)
+	{
+		if(head==null || head.next==null)
+		{
+			return null;
+		}
+		
+		if(pos==1)
+		{
+			head=head.next;
+			return head;
+		}
+		
+		Node q=head;
+		for(int i=1;i<pos-1 && q!=null;i++)
+		{
+			q=q.next;
+		}
+	
+		q.next=q.next.next;
+		return head;
+		
+	}
+	
+	public int sizeOfList(Node head)
+	{
+		if(head==null)
+			return 0;
+		
+		return sizeOfList(head.next)+1;
+	}
+	
+	public boolean searchNode(Node head,int x)
+	{
+		if(head==null)
+			return false;
+		
+		if(head.x==x)
+			return true;
+		
+		return searchNode(head.next,x);
+	}
+	
 	public void display(Node head)
 	{
 		Node q=head;
@@ -55,7 +98,6 @@ class Linked
 	}
 	
 }
-
 public class LinkedList
 {
 	public static void main(String args[])
@@ -69,15 +111,19 @@ public class LinkedList
 		head=a.insert(5,4,head);
 		head=a.insert(4,4,head);
 		head=a.insert(0,1,head);
-		head=a.insert(05,2,head);
-		head=a.insert(6,9,head);
+		//head=a.insert(05,2,head);
+		//head=a.insert(6,9,head);
 		
-		
-		
-
-
+		head=a.delete(head,1);
+		head=a.delete(head,5);
 		
 		a.display(head);
 		
+		System.out.println("Size of Linked List = "+a.sizeOfList(head));
+		System.out.println("Search = "+a.searchNode(head,2));
+		
+	
+		
 	}
 }
+
