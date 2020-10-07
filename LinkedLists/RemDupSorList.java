b06fc85601b44d28ff04edd91ca9bbc68d7349ae
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-07 19:24:11 +0530

    83. Remove Duplicates from Sorted List - Leetcode Java Recursive Solution

--- LinkedLists/RemDupSorList.java
+++ LinkedLists/RemDupSorList.java
@@ -45,3 +45,41 @@ class RemDupSorList
         return head;
     }
 }
+
+
+/*-------------------------Recursive Solution---------------------*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/*
+class Solution
+{
+    public ListNode deleteDuplicates(ListNode head)
+    {
+        if(head==null)
+            return null;
+        
+        if(head.next!=null)
+        {
+            if (head.val==head.next.val) 
+            {  
+                head.next = head.next.next;
+                deleteDuplicates(head);
+            }
+            else
+                deleteDuplicates(head.next);
+         
+        }
+        
+        return head;
+    }
+}
+*/
