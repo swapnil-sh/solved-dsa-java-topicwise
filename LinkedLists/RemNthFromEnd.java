6e4ade19f22c922daf7241900e0f4cc39b70a8b5
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-16 18:46:41 +0530

    19. Remove Nth Node From End of List - Leetcode Java One Pass Solution

--- LinkedLists/RemNthFromEnd.java
+++ LinkedLists/RemNthFromEnd.java
@@ -1,4 +1,4 @@
-/*19. Remove Nth Node From End of List - Leetcode Java Solution*/
+/*19. Remove Nth Node From End of List - Leetcode Java One Pass Solution*/
 
 /**
  * Definition for singly-linked list.
@@ -49,3 +49,33 @@ class RemNthFromEnd
         return head;   
     }
 }
+
+
+
+/*
+
+public ListNode removeNthFromEnd(ListNode head, int n) {
+    
+    ListNode start = new ListNode(0);
+    ListNode slow = start, fast = start;
+    slow.next = head;
+    
+    //Move fast in front so that the gap between slow and fast becomes n
+    for(int i=1; i<=n+1; i++)   {
+        fast = fast.next;
+    }
+    //Move fast to the end, maintaining the gap
+    while(fast != null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+    //Skip the desired node
+    slow.next = slow.next.next;
+    return start.next;
+}
+*/
+
+
+
+
+
