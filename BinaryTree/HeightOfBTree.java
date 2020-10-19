1f0cec783f51cabeda509d9ed262fc30ea55efe9
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-10-19 20:48:22 +0530

    Height of Binary Tree - GeeksforGeeks Recursive and Iterative Java Solution

---
+++ BinaryTree/HeightOfBTree.java
@@ -0,0 +1,77 @@
+/*Height of Binary Tree - GeeksforGeeks Recursive and Iterative Java Solution*/
+
+
+//-----------------------------Iterative Solution using BFS-Queue------------------------------
+
+/* 
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+
+class HeightOfBTree 
+{
+    int height(Node node) 
+    {
+        // code here 
+        
+        if(node==null)
+            return 0;
+        
+        int i,levelNo=0,h=0;    
+        Queue<Node> q=new LinkedList<>();
+        
+        q.offer(node);
+        Node temp=null;
+        
+        while(q.size()>0)
+        {
+            levelNo=q.size();
+            for(i=0;i<levelNo;i++)
+            {
+                temp=q.peek();
+                if(temp.left!=null)
+                    q.offer(temp.left);
+                
+                if(temp.right!=null)
+                    q.offer(temp.right);
+                    
+                q.remove();
+            }
+            
+            h++;
+        }
+        
+        return h;
+    }
+}
+
+
+
+//-----------------------------Recursive Solution------------------------------
+
+/*
+class Solution 
+{
+    int height(Node node) 
+    {
+        // code here 
+        
+        if(node==null)
+            return 0;
+            
+        return Math.max(height(node.left),height(node.right))+1;
+    }
+}
+
+*/
+
