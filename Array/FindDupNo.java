e02bd276c75f76ecb7a3f7b88c2c3a728be1355c
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-02 18:46:00 +0530

    Added Comments about all approaches

--- Array/FindDupNo.java
+++ Array/FindDupNo.java
@@ -1,5 +1,14 @@
 /*287. Find the Duplicate Number - Leetcode Java Solution using Floyds Cycle Detetction Algo*/
 
+
+/*
+	TC - O(n)
+	SC - O(1)
+
+Other Approaches - 
+	1. Sort and find next TC-O(nlogn) and SC-O(1) but array will be modified
+	2. Use Hashing TC-O(n) and SC-O(n)
+*/
 class FindDupNo
 {
     public int findDuplicate(int[] a) 
