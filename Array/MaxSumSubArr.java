30cbe73a932d30268b1e140d4e5169c3398c5a1e
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-04 00:34:01 +0530

    Added Brute Force Solution

--- Array/MaxSumSubArr.java
+++ Array/MaxSumSubArr.java
@@ -29,3 +29,40 @@ class MaxSumSubArr
         return ans;
     }
 }
+
+//-------------------------------Brute Force Solution------------------------------------
+/*
+
+TC = O(n^3)
+SC = O(1)
+
+*/
+/*
+class Solution 
+{
+    public int maxSubArray(int[] a)
+    {
+        if(a.length==1)
+            return a[0];
+        
+        int ans=Integer.MIN_VALUE,currSum=0,i,j,k;
+        int n=a.length;
+        for(i=0;i<n;i++)
+        {
+            for(j=i;j<n;j++)
+            {
+                currSum=0;
+                for(k=i;k<=j;k++)
+                {
+                    currSum+=a[k];
+                }
+                
+                 ans=Math.max(ans,currSum); 
+            }
+        }
+        
+        return ans;
+    }
+}
+
+*/
