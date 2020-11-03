30ab83ffb8b18a5aaaf05a15b8582eb00bcab0cc
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-04 00:49:13 +0530

    Added O(n^2) solution and Follow up questions

--- Array/MaxSumSubArr.java
+++ Array/MaxSumSubArr.java
@@ -30,6 +30,32 @@ class MaxSumSubArr
     }
 }
 
+//-------------------------------O(n^2) Solution----------------------------------------
+/*
+class Solution 
+{
+    public int maxSubArray(int[] a)
+    {
+        if(a.length==1)
+            return a[0];
+        
+        int ans=Integer.MIN_VALUE,currSum=0,i,j;
+        int n=a.length;
+        for(i=0;i<n;i++)
+        {
+            currSum=0;
+            for(j=i;j<n;j++)
+            {
+                currSum+=a[j]; 
+                ans=Math.max(ans,currSum);
+            }
+        }
+        
+        return ans;
+    }
+}
+*/
+
 //-------------------------------Brute Force Solution------------------------------------
 /*
 
@@ -64,5 +90,26 @@ class Solution
         return ans;
     }
 }
+
+*/
+
+/*
+
+Follow up 1: If you can choose to remove one value in the given list, what's the maximum subarray sum you can get.
+
+Input : [1,-1,3]
+Output : [4] 
+Explain : you will want to remove  -1, and you get a new array [1,3]
+
+Input : [1,1,3] 
+Output : [5]
+Explain : you will not want to remove anything
+
+Follow up 2 : if the array is too large to fit into the memory and you have a multi-core CPU. How can you speed things up?
+Is that possible to run different thread on different part of given array and put thing together to find a solution?
+(This question doesn't related to follow up 1)
+
+Just a fruitful thought. I was asked in the interview. Hope it can help.
+
 
 */
