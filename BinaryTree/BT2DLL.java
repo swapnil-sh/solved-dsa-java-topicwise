1853b2da2c3e944d83cba14552be503eee612d64
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-06 16:14:35 +0530

    Added another solution of geeksforgeeks

--- BinaryTree/BT2DLL.java
+++ BinaryTree/BT2DLL.java
@@ -28,3 +28,81 @@ class BT2DLL
 	   return head; 
     }
 }
+
+
+//-------------------Reverse Recursive Approach---------------------
+/*
+class GfG
+{
+    Node head;
+    Node bToDLL(Node root) 
+    { 
+        // Base cases 
+        if (root == null) 
+            return null; 
+  
+        // Recursively convert right subtree 
+        bToDLL(root.right); 
+  
+        // insert root into DLL 
+        root.right = head; 
+  
+        // Change left pointer of previous head 
+        if (head != null) 
+            head.left = root; 
+  
+        // Change head of Doubly linked list 
+        head = root; 
+  
+        // Recursively convert left subtree 
+        bToDLL(root.left); 
+        
+        return head;
+    } 
+}
+*/
+
+
+//------------------------Recursive Apporach three fixing left and right pointers seperately---------------------------------
+/*
+class GfG
+{
+    static Node prev;
+    void setLeftPtr(Node root)
+    {
+        if(root==null)
+            return;
+        
+        setLeftPtr(root.left);
+        root.left=prev;
+        prev=root;
+        setLeftPtr(root.right);
+    }
+    
+    Node setRightPtr(Node root)
+    {
+        while(root.right!=null)
+            root=root.right;
+        
+        while(root!=null && root.left != null)
+        {
+            Node left=root.left;
+            left.right=root;
+            root=left;
+        }
+        
+        return root;
+    }
+    Node bToDLL(Node root) 
+    { 
+        // Base cases 
+        if (root == null) 
+            return null; 
+        
+        prev = null; 
+  
+        setLeftPtr(root); 
+        return setRightPtr(root);
+    } 
+}
+*/
