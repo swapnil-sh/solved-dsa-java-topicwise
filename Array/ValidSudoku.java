e0a11f8fa214ce18b86078030ecf4e345ddcff3f
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-11 13:06:57 +0530

    36. Valid Sudoku - Leetcode Java Solution

--- Array/ValidSudoku.java
+++ Array/ValidSudoku.java
@@ -35,3 +35,45 @@ class ValidSudoku
         return true;
     }
 }
+
+
+/*
+class Solution 
+{
+    public boolean isValidSudoku(char[][] board) 
+    {
+        boolean[][] row=new boolean[9][9];
+        boolean[][] col=new boolean[9][9];
+        boolean[][] box=new boolean[9][9];
+        
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]=='.') 
+                    continue;
+                
+                
+                if(row[i][board[i][j]-'1'])
+                    return false;
+                else
+                    row[i][board[i][j]-'1']=true;
+                
+                
+                if(col[j][board[i][j]-'1'])
+                    return false;
+                else
+                    col[j][board[i][j]-'1']=true;
+                
+                
+                if(box[(i/3)*3+j/3][board[i][j]-'1'])
+                    return false;
+                else
+                    box[(i/3)*3+j/3][board[i][j]-'1']=true;
+                
+            }
+        }
+        return true;
+    }
+}
+*/
