278f397b9b296c9093c43117989596e5f58058e7
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-12 16:55:26 +0530

    Added Inplace Solution

--- LinkedLists/Merge2SorLists.java
+++ LinkedLists/Merge2SorLists.java
@@ -1,4 +1,14 @@
-/*21. Merge Two Sorted Lists - Leetcode Java Solution*/
+/*21. Merge Two Sorted Lists - Leetcode Java Solutions*/
+
+
+
+/*
+----------------------------Inplace Solution without using Dummmy Node-----------------------
+
+TC - O(n+m)
+SC - O(1)
+
+*/
 
 /**
  * Definition for singly-linked list.
@@ -10,6 +20,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
+class Merge2SorLists
+{
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) 
+    {
+        if(l1==null && l2==null)
+            return null;
+        
+        if(l1==null && l2!=null)
+            return l2;
+        
+        if(l1!=null && l2==null)
+            return l1;
+        
+        if(l1.val>l2.val)
+        {
+            ListNode t=l1;
+            l1=l2;
+            l2=t;
+        }
+        ListNode res=l1;
+        
+        while(l1!=null && l2!=null)
+        {
+            ListNode t1=null;
+            while(l1!=null && l1.val<=l2.val)
+            {
+                t1=l1;
+                l1=l1.next;
+            }
+            
+            t1.next=l2;
+            
+            ListNode t2=l1;
+            l1=l2;
+            l2=t2;
+        }
+        
+        return res;
+    }
+}
+
+/*--------------------------------Using DymmyNode--------------------------------*/
+
+/*
 class Merge2SorLists 
 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2)
@@ -49,3 +105,4 @@ class Merge2SorLists
         
     }
 }
+*/
