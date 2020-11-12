cd35774ac42be8f2a7ecc6a5b06090fc75bd1bb9
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-11-12 21:30:10 +0530

    Added one more solution using reverse function

--- LinkedLists/CheckPalinLL.java
+++ LinkedLists/CheckPalinLL.java
@@ -51,3 +51,63 @@ class CheckPalinLL
   
     }
 }
+
+
+
+/*
+----------------------Using Reverse Function------------------------s
+*/
+
+/*
+class Solution
+{
+    public ListNode reverse(ListNode head)
+    {
+        ListNode curr=head;
+        ListNode prev=null;
+        ListNode next=null;
+        
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        
+        head=prev;
+        
+        return head;
+    }
+    public boolean isPalindrome(ListNode head) 
+    {
+        
+        if(head==null || head.next==null)
+            return true;
+        
+        ListNode slow=head;
+        ListNode fast=head;
+        
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        slow.next=reverse(slow.next);
+        
+        slow=slow.next;
+        
+        while(slow!=null)
+        {
+            if(head.val!=slow.val)
+                return false;
+            head=head.next;
+            slow=slow.next;
+        }
+        
+        return true;
+        
+    }
+}
+*/
