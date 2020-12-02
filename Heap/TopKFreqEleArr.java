ca2033a2e574ba737f76db228d5e2d2d54776d72
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-02 18:02:54 +0530

    Added Quick Select Solution

--- Heap/TopKFreqEleArr.java
+++ Heap/TopKFreqEleArr.java
@@ -7,6 +7,7 @@ class TopKFreqEleArr
         
         ArrayList<Integer> res=new ArrayList<>();
         Map<Integer,Integer> hmap=new HashMap<>();
+
         PriorityQueue<Map.Entry<Integer, Integer>> minHeap = new PriorityQueue<>(
         (n1, n2) -> n1.getValue() - n2.getValue());
         
@@ -33,3 +34,79 @@ class TopKFreqEleArr
         return res.stream().mapToInt(j -> j).toArray();
     }
 }
+
+/*
+-----------------------Using Quick Select-------------------------------
+*/
+
+/*
+class Solution 
+{
+    HashMap<Integer,Integer> freq;
+    public int[] topKFrequent(int[] nums, int k)
+    {
+        freq = new HashMap<Integer,Integer>();
+        
+        for(int i=0;i<nums.length;i++)
+            freq.put(nums[i], freq.getOrDefault(nums[i],0)+1);
+        
+        
+        int[] elems = new int[freq.size()];
+        int index = 0;
+        
+        for(int num : freq.keySet())
+            elems[index++] = num;
+        
+        quickSelect(elems, 0, elems.length-1, k);
+        int res[]=new int[k];
+        
+        for(int i=0; i<k; i++)
+            res[i]=elems[i];
+        
+        return res;
+    }
+    
+    public void quickSelect(int[] elems, int lo, int hi, int k)
+    {
+        if(lo<hi)
+        {
+            int mid = partition(elems, lo, hi);
+            if(mid==k)
+                return;
+            
+            if(mid<k)
+                quickSelect(elems, mid+1, hi, k);
+            
+            else
+                quickSelect(elems, lo, mid-1, k);
+        }
+    }
+    
+    public int partition(int[] elems, int lo, int hi)
+    {
+        int pivotIndex = lo + (int)((hi-lo+1)*Math.random());
+        swap(elems, pivotIndex, hi);
+        
+        int pivotFreq = freq.get(elems[hi]);
+        int j = 0;
+        
+        for(int i=0; i<hi; i++)
+        {
+            if(freq.get(elems[i])>=pivotFreq)
+                swap(elems, i, j++);
+            
+        }
+        swap(elems, j, hi);
+        return j;
+    }
+    
+    public void swap(int[] elems, int i, int j)
+    {
+        int temp = elems[i];
+        elems[i] = elems[j];
+        elems[j] = temp;
+    }
+    
+    
+}
+*/
