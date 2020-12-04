4304608b81585aa8a704110c2e239225cf9153b2
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-04 20:46:19 +0530

    700. Search in a Binary Search Tree - Leetcode added iterative solution

--- BinarySearchTree/SearchInBST.java
+++ BinarySearchTree/SearchInBST.java
@@ -38,3 +38,31 @@ class SearchInBST
         return root;
     }
 }
+
+
+/*
+------------------Iterative Solution--------------------
+
+TC - O(n)
+SC - O(1)
+*/
+
+/*
+class Solution
+{
+    public TreeNode searchBST(TreeNode root, int val)
+    {
+        while(root!=null)
+        {
+            if(root.val>val)
+                root=root.left;
+            else if(root.val<val)
+                root=root.right;
+            else
+                break;
+        }
+            
+        return root;
+    }
+}
+*/
