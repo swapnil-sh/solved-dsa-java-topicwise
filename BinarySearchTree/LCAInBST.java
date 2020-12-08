4d784b90e37d5fc67ecc3565349ec63ae53ee0c0
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-08 15:27:40 +0530

    Lowest Common Ancestor in a BST - GeeksforGeeks Java Solutions added Iterative Solution

--- BinarySearchTree/LCAInBST.java
+++ BinarySearchTree/LCAInBST.java
@@ -42,3 +42,39 @@ class LCAInBST
         }
     
 }
+
+/*
+--------------Iterative Solution-------------------
+
+Time Complexity: O(Height of the BST).
+Auxiliary Space: O(1).
+*/
+
+/*
+class BST
+{   
+   
+	Node LCA(Node root, int n1, int n2)
+	{
+        
+        while (root != null)  
+        {  
+            // If both n1 and n2 are smaller  
+            // than root, then LCA lies in left  
+            if(root.data > n1 && root.data > n2)  
+                root = root.left;  
+      
+            // If both n1 and n2 are greater  
+            // than root, then LCA lies in right  
+            else if(root.data < n1 &&  root.data < n2)  
+                root = root.right;  
+      
+            else 
+                break;  
+        }  
+        
+        return root;   
+    }
+    
+}
+*/
