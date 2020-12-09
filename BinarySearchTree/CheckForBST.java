de8d249ac8a0d665b18b5a905bc2dc5c067a8da1
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-09 23:02:46 +0530

    Check for BST - GeeksforGeeks Java Solutions added optimized solution

--- BinarySearchTree/CheckForBST.java
+++ BinarySearchTree/CheckForBST.java
@@ -1,11 +1,41 @@
 /*Check for BST - GeeksforGeeks Java Solutions*/
 
 /*
+---------------------Using Range check(min and max values)---------------------
+
+TC - O(n)
+SC - O(n)
+*/
+
+class CheckForBST
+{
+    // return true if the given tree is a BST, else return false
+    
+    boolean solve(Node root, int min, int max)
+    {
+        if(root==null)
+            return true;
+        
+        if(root.data<min || root.data>max)
+            return false;
+        
+        return (solve(root.left, min, root.data-1) && solve(root.right, root.data+1, max));
+    }
+    boolean isBST(Node root)
+    {
+            // code here.
+        return solve(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+}
+
+
+/*
 ---------------------Using Max and Min Value---------------------
 
 TC - O(n^2)
 SC - O(n)
 */
+/*
 class CheckForBST
 {
     // return true if the given tree is a BST, else return false
@@ -53,3 +83,5 @@ class CheckForBST
         
     }
 }
+
+*/
