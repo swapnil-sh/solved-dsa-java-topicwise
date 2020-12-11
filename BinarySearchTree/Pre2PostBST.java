4d1ab78e739572a40394a3b07db3d78620b3f8ef
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-11 22:33:33 +0530

    Preorder to Postorder  - GeeksforGeeks Java Solution (Construct BST from preorder)

--- BinarySearchTree/Pre2PostBST.java
+++ BinarySearchTree/Pre2PostBST.java
@@ -10,33 +10,26 @@ class Pre2PostBST
 {
     public static Node constructTree(int pre[], int size) 
     {
-    //Your code here
-        Node root = new Node(pre[0]); 
     
+        Node root = new Node(pre[0]); 
         Stack<Node> s = new Stack<Node>(); 
   
-        // Push root 
         s.push(root); 
         int i;
+
         for(i=1;i<size;i++) 
         { 
             Node temp = null; 
   
             while (!s.isEmpty() && pre[i]>s.peek().data)
                 temp = s.pop(); 
-            
-  
-            // Make this greater value as the right child 
-            // and push it to the stack 
-            if (temp != null) 
+
+            if (temp!=null) 
             { 
                 temp.right = new Node(pre[i]); 
                 s.push(temp.right); 
             }  
-              
-            // If the next value is less than the stack's top 
-            // value, make this value as the left child of the  
-            // stack's top node. Push the new node to stack 
+             
             else
             { 
                 temp = s.peek(); 
