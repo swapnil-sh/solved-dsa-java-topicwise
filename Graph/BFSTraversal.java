22652bdce8d452fcd9c4adcad2955a5729ba7227
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-12 13:29:37 +0530

    BFS of graph  - GeeksforGeeks Java Solution

---
+++ Graph/BFSTraversal.java
@@ -0,0 +1,42 @@
+/* BFS of graph  - GeeksforGeeks Java Solution*/
+
+/*
+----------------------Using Stack--------------------
+
+Time Complexity: O(V + E).
+Auxiliary Space: O(V).
+*/
+
+class BFSTraversal
+{
+    static ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> g, int N)
+    {
+        // add your code here
+        ArrayList<Integer> res=new ArrayList<>();
+        if(g.size()==0)
+            return res;
+        
+        boolean vis[]=new boolean[N];
+        Queue<Integer> q=new LinkedList<>();
+        
+        q.offer(0);
+        vis[0]=true;
+        
+        while(!q.isEmpty())
+        {
+            int temp=q.poll();
+            res.add(temp);
+        
+            for(int i:g.get(temp))
+            {
+                if(!vis[i])
+                {
+                    vis[i]=true;
+                    q.offer(i);
+                }
+            }
+        }
+        return res;
+        
+    }
+}
