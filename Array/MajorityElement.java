f82bdb2325262ab3ff9a3c485711db0715415655
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-16 12:11:19 +0530

    169. Majority Element - Leetcode Java Solutions

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -30,3 +30,57 @@ class MajorityElement
         
     }
 }
+
+/*
+----------------------Using Sorting-----------------
+
+TC - O(nlogn)
+SC - O(1)
+*/
+/*
+class Solution
+{
+    public int majorityElement(int[] a)
+    {
+        if(a.length==0)
+            return 0;
+        Arrays.sort(a);
+        
+        return a[a.length/2];
+        
+    }
+}
+*/
+
+
+/*
+----------------------Brute Force---------------------------
+
+TC - O(n^2)
+SC - O(1)
+*/
+/*
+class Solution
+{
+    public int majorityElement(int[] a)
+    {
+        if(a.length==0)
+            return -1;
+        
+        int i,j,count=0;
+        for(i=0;i<a.length;i++)
+        {
+            count=0;
+            for(j=i+1;j<a.length;j++)
+            {
+                if(a[i]==a[j])
+                    count++;
+            }
+            
+            if(count>=a.length/2)
+                return a[i];
+        }
+        return -1;
+    }
+}
+*/
