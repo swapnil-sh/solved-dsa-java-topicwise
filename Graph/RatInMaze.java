42ebd95af0297bd61f1b5c78ce4c598b801b469f
swapnil-sh <swapnil.sharma2709@gmail.com>
2020-12-29 23:54:20 +0530

    Rat in a Maze Problem - I: GeeksforGeeks Java Solution using Backtracking

---
+++ Graph/RatInMaze.java
@@ -0,0 +1,34 @@
+/*Rat in a Maze Problem - I: GeeksforGeeks Java Solution using Backtracking*/
+
+class RatInMaze
+{
+    private static void findPathInMaze(int m[][], int n, ArrayList<String> res, int i, int j, String path)
+    {
+        if(i<0 || i>=n || j<0 || j>=n || m[i][j]==0)
+            return;
+        
+        if(i==n-1 && j==n-1)
+        {
+            res.add(path);
+            return;
+        }
+        
+        m[i][j]=0;
+        
+        findPathInMaze(m,n,res,i+1,j,path+"D");
+        findPathInMaze(m,n,res,i,j-1,path+"L");
+        findPathInMaze(m,n,res,i,j+1,path+"R");
+        findPathInMaze(m,n,res,i-1,j,path+"U");
+        
+        m[i][j]=1;
+        
+    }
+    public static ArrayList<String> printPath(int[][] m, int n) 
+    {
+        ArrayList<String> res=new ArrayList<>();
+        String path="";
+        
+        findPathInMaze(m,n,res,0,0,path);
+        return res;
+    }
+}
