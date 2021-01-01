1599b6a1dcd3bc350cc5e27a8d60fbcbfffc8eeb
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-01-01 21:45:11 +0530

    Check Array Formation Through Concatenation - Java Leetcode Solutions

---
+++ LeetCodeDailyChall/JanurayChallenge_2021/ChechArrFormConcat.java
@@ -0,0 +1,39 @@
+/*Check Array Formation Through Concatenation - Java Leetcode Solutions*/
+
+
+class ChechArrFormConcat
+{
+    public boolean canFormArray(int[] a, int[][] pieces) 
+    {
+        
+        Map<Integer, int[]> map = new HashMap<>();
+        for (int[] piece : pieces)
+            map.put(piece[0], piece);
+        
+        // for(Map.Entry hm : map.entrySet())
+        //     System.out.println(hm.getKey()+" "+hm.getValue());
+
+        int i=0;
+        while(i<a.length) 
+        {
+            int curVal = a[i];
+            if (map.containsKey(curVal)) 
+            {
+                int[] piece = map.get(curVal);
+                for (int value : piece) 
+                {
+                    if (a[i] == value) 
+                        i++;
+                    else 
+                        return false;
+                }
+            }
+            
+            else
+                return false;
+        }
+        
+        return true;
+        
+    }
+}
