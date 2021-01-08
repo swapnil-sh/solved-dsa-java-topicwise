684e8553079fa118dd6053610f7150b376105687
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-01-08 17:26:11 +0530

    3. Longest Substring Without Repeating Characters - Leetcode Java Solutions

--- Strings/LongSubsWithoutRepChars.java
+++ Strings/LongSubsWithoutRepChars.java
@@ -1,5 +1,52 @@
 /*3. Longest Substring Without Repeating Characters - Leetcode Java Solutions*/
 
+
+/*
+---------------------------Sliding Window Approach (Optimised)----------------------------
+
+Time Complexity : O(n)
+Space Complexity :  O(min(n,m))
+
+*/
+
+class LongSubsWithoutRepChars
+{
+   
+    public int lengthOfLongestSubstring(String s) 
+    {
+        if(s.length()==0)
+            return 0;
+        
+        if(s.length()==1)
+            return 1;
+        
+        int i=0,j=0, res=0,n=s.length();
+        HashMap<Character, Integer> hm = new HashMap<>();
+        
+        while(j<n)
+        {
+            if(hm.containsKey(s.charAt(j)))
+                i=Math.max(hm.get(s.charAt(j))+1, i);
+            
+            hm.put(s.charAt(j), j);
+            res=Math.max(res, j-i+1);
+            j++;
+        }   
+        
+        return res;
+    }
+}
+
+/*
+---------------------------Sliding Window Approach----------------------------
+
+Time Complexity : O(2n)
+Space Complexity :  O(min(n,m))
+
+*/
+
+
+/*
 class LongSubsWithoutRepChars
 {
    
@@ -33,8 +80,16 @@ class LongSubsWithoutRepChars
         return res;
     }
 }
+*/
 
 
+/*
+---------------------------Brute Force----------------------------
+
+Time Complexity : O(n^3)
+Space Complexity :  O(min(n,m))
+
+*/
 
 /*
 class Solution
