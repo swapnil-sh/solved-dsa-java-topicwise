08f5fd938e44558fcc838bcfa0982aaa41b03c44
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-01-11 12:20:48 +0530

    Word Break - GeeksforGeeks Java Solutions added Tabulation solution

--- DynamicProgramming/WordBreak.java
+++ DynamicProgramming/WordBreak.java
@@ -1,9 +1,74 @@
 /*Word Break - GeeksforGeeks Java Solutions*/
 
+
+/*
+--------------------Tabulation Solution-------------------------------
+
+*/
+
+class WordBreak
+{
+    private static int solve(String s, Set<String> dict)
+    {
+        int T[][] = new int[s.length()][s.length()];
+        
+        for(int i=0; i < T.length; i++)
+        {
+            for(int j=0; j < T[i].length ; j++)
+            {
+                T[i][j] = -1; //-1 indicates string between i to j cannot be split
+            }
+        }
+        
+        //fill up the matrix in bottom up manner
+        for(int l = 1; l <= s.length(); l++)
+        {
+            for(int i=0; i < s.length() -l + 1 ; i++)
+            {
+                int j = i + l-1;
+                String str = s.substring(i,j+1);
+                //if string between i to j is in dictionary T[i][j] is true
+                if(dict.contains(str))
+                {
+                    T[i][j] = i;
+                    continue;
+                }
+                //find a k between i+1 to j such that T[i][k-1] && T[k][j] are both true 
+                for(int k=i+1; k <= j; k++)
+                {
+                    if(T[i][k-1] != -1 && T[k][j] != -1)
+                    {
+                        T[i][j] = k;
+                        break;
+                    }
+                }
+            }
+            
+        }
+        
+        if(T[0][s.length()-1] == -1)
+            return 0;
+        else
+            return 1;
+    }
+    
+    public static int wordBreak(String s, ArrayList<String> dict)
+    {
+        //code here
+        Set<String> hs=new HashSet<>();
+        int i;
+        
+        for(i=0;i<dict.size();i++)
+            hs.add(dict.get(i));
+        
+        return solve(s,hs);
+    }
+}
 /*
 --------------------Memoization Solution-------------------------------
 
 */
+/*
 class WordBreak
 {
     static HashMap<String, Integer> dp=new HashMap<>();
@@ -41,7 +106,7 @@ class WordBreak
         return solve(s,hs);
     }
 }
-
+*/
 /*
 --------------------Recursive Solution-------------------------------
 
