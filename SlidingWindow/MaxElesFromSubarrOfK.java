09ebfcdcad5a5427e95d8eade14275b99c6a740a
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-01-13 18:11:58 +0530

    Maximum of all subarrays of size k - GeeksforGeeks Java Solutions */

--- SlidingWindow/MaxElesFromSubarrOfK.java
+++ SlidingWindow/MaxElesFromSubarrOfK.java
@@ -1,10 +1,77 @@
 /*Maximum of all subarrays of size k - GeeksforGeeks Java Solutions */
 
+
+/*
+/*
+----------------------------Sliding Window and Dequeue-----------------------
+TC - O(n)
+SC - O(k)
+*/
+
+*/
+class MaxElesFromSubarrOfK
+{
+    static ArrayList <Integer> max_of_subarrays(int a[], int n, int k)
+    {
+        // Your code here
+        int i,j,max=Integer.MIN_VALUE;
+        ArrayList<Integer> res=new ArrayList<>();
+        
+       i=0;
+       j=0;
+       
+       Deque<Integer> dq = new LinkedList<Integer>();
+       while(j<n)
+       {
+            while(!dq.isEmpty() && a[dq.peekLast()]<=a[j])
+                dq.removeLast();
+            
+            dq.addLast(j);
+            
+            if(j-i+1<k)
+                j++;
+                
+            else if(j-i+1==k)
+            {
+                if(!dq.isEmpty() && dq.peekFirst()<i)
+                    dq.removeFirst();
+                    
+                res.add(a[dq.peekFirst()]);
+                    
+                j++;
+                i++;
+            }
+        }
+    //   while(j<n)
+    //   {
+    //         while (!dq.isEmpty() && a[j]>=dq.peekLast())
+    //             dq.removeLast();
+            
+    //         dq.addLast(a[j]);
+            
+    //         if(j-i+1==k)
+    //         {
+    //             res.add(dq.peekFirst());
+    //             if(!dq.isEmpty() && dq.peekFirst()==a[i])
+    //                 dq.removeFirst();
+                
+    //             i++;
+    //             j++;
+    //         }
+    //         else
+    //             j++;
+    //   }
+        
+        
+        return res;
+    }
+}
 /*
 ----------------------------Brute Force------------------------
 TC - O(n*k)
 SC - O(1)
 */
+/*
 class MaxElesFromSubarrOfK
 {
     static ArrayList <Integer> max_of_subarrays(int a[], int n, int k)
@@ -26,4 +93,4 @@ class MaxElesFromSubarrOfK
         
         return res;
     }
-}
+}*/
