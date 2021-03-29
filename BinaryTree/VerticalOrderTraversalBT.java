b5f7386a013c904020e59f8fec31d10e2d445a60
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-03-29 13:37:45 +0530

    Vertical Order Traversal of a Binary Tree - Added GeeksforGeeks solutions

--- BinaryTree/VerticalOrderTraversalBT.java
+++ BinaryTree/VerticalOrderTraversalBT.java
@@ -86,7 +86,7 @@ class VerticalOrderTraversalBT
 
 
 
-//--------------------------------------Recursive Code (Coding Simplified)----------------------------------------
+//--------------------------------------Recursive Code - Using Preorder Traversal and TreeMap(Coding Simplified)----------------------------------------
 /*
   public void printVerticalOrder(Node node) {
     if (node == null) {
@@ -192,4 +192,89 @@ class BinaryTree
     }
 }
 
+*/
+
+
+/*
+----------------------------------------GFG Recursive MinMax Solution---------------------------------
+
+Time Complexity: Time complexity of below algorithm is O(w*n) where w is width of Binary Tree and n is number of nodes in Binary Tree.
+In worst case, the value of w can be O(n) (consider a complete tree for example) and time complexity can become O(n2).
+*/
+
+/*
+class Node 
+{
+    int data;
+    Node left, right;
+   
+    Node(int item) 
+    {
+        data = item;
+        left = right = null;
+    }
+}
+   
+class Values 
+{
+    int max, min;
+}
+   
+class BinaryTree 
+{
+    Node root;
+    Values val = new Values();
+   
+    // A utility function to find min and max distances with respect
+    // to root.
+    void findMinMax(Node node, Values min, Values max, int hd) 
+    {
+        // Base case
+        if (node == null) 
+            return;
+   
+        // Update min and max
+        if (hd < min.min) 
+            min.min = hd;
+        else if (hd > max.max) 
+            max.max = hd;
+   
+        // Recur for left and right subtrees
+        findMinMax(node.left, min, max, hd - 1);
+        findMinMax(node.right, min, max, hd + 1);
+    }
+   
+    // A utility function to print all nodes on a given line_no.
+    // hd is horizontal distance of current node with respect to root.
+    void printVerticalLine(Node node, int line_no, int hd) 
+    {
+        // Base case
+        if (node == null) 
+            return;
+   
+        // If this node is on the given line number
+        if (hd == line_no) 
+            System.out.print(node.data + " ");        
+   
+        // Recur for left and right subtrees
+        printVerticalLine(node.left, line_no, hd - 1);
+        printVerticalLine(node.right, line_no, hd + 1);
+    }
+   
+    // The main function that prints a given binary tree in
+    // vertical order
+    void verticalOrder(Node node) 
+    {
+        // Find min and max distances with resepect to root
+        findMinMax(node, val, val, 0);
+   
+        // Iterate through all possible vertical lines starting
+        // from the leftmost line and print nodes line by line
+        for (int line_no = val.min; line_no <= val.max; line_no++) 
+        {
+            printVerticalLine(node, line_no, 0);
+            System.out.println("");
+        }
+    }
+   
 */
