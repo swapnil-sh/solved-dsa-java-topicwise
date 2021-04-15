26f072272033e6bcdd0be37f9955be40fb56c8c2
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-04-15 20:43:19 +0530

    617. Merge Two Binary Trees - Leetcode Java Solutions

--- BinaryTree/MergeTwoBT.java
+++ BinaryTree/MergeTwoBT.java
@@ -3,7 +3,7 @@
 /*
 ----------------Recursive - (DFS,Preorder) Solution--------------------------------
 TC - O(n)
-TC - O(h)
+TC - O(n) (For Skewed Tree)
 */
 
 class MergeTwoBT
@@ -46,3 +46,73 @@ class MergeTwoBT
         
     }
 }
+
+/*
+----------------Same Recursive Better coded - (DFS,Preorder) Solution--------------------------------
+TC - O(n)
+TC - O(n) (For Skewed Tree)
+*/
+
+/*
+class Solution
+{
+    private TreeNode solve(TreeNode t1, TreeNode t2)
+    {
+       if(t1==null)
+            return t2;
+        
+        if(t2==null)
+            return t1;
+        
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        
+        return t1;
+    }
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2)
+    {
+        return solve(root1, root2);
+        
+    }
+}
+*/
+
+
+/*
+---------------Iterative Solution using Stack---------------------------------
+TC - O(n)
+TC - O(n) (For Skewed Tree)
+*/
+/*
+public class Solution 
+{
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) 
+    {
+        if (t1 == null)
+            return t2;
+        Stack < TreeNode[] > stack = new Stack < > ();
+        stack.push(new TreeNode[] {t1, t2});
+        while (!stack.isEmpty()) 
+        {
+            TreeNode[] t = stack.pop();
+            if (t[0] == null || t[1] == null) 
+                continue;
+            
+            t[0].val += t[1].val;
+            if (t[0].left == null) 
+                t[0].left = t[1].left;
+            else 
+                stack.push(new TreeNode[] {t[0].left, t[1].left});
+            
+            if (t[0].right == null)
+                t[0].right = t[1].right;
+            else 
+                stack.push(new TreeNode[] {t[0].right, t[1].right});
+            
+        }
+        return t1;
+    }
+}
+
+*/
