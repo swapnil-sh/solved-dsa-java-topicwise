c4e8b82bab6f67baa1fd69af232af7199c933fc0
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-04-20 18:37:13 +0530

    11. Container With Most Water - Leetcode Java Solutions

---
+++ TwoPointers/ContWithMostWater.java
@@ -0,0 +1,53 @@
+/*11. Container With Most Water - Leetcode Java Solutions*/
+
+/*
+------------------------Brute Force-----------------------
+TC - O(n^2)
+SC - O(1)
+*/
+
+class ContWithMostWater
+{
+    public int maxArea(int[] a)
+    {
+        int i=0,j=a.length-1,res=0,ar=0;
+
+        while(i<j) 
+        {
+            ar=(j-i)*(Math.min(a[i], a[j]));
+            res=Math.max(ar, res);
+            
+            if(a[i]<=a[j])
+                i++;
+            else
+                j--;
+        }
+        
+        return res;
+    }
+}
+
+/*
+------------------------Brute Force-----------------------
+TC - O(n^2)
+SC - O(1)
+*/
+/*
+class Solution 
+{
+    public int maxArea(int[] a)
+    {
+        int i,j,res=0,ar=0;
+
+        for(i=0;i<a.length-1;i++)
+        {
+            for(j=i+1;j<a.length;j++)
+            {
+                ar=(j-i)*Math.min(a[i], a[j]);
+                res=Math.max(ar, res);
+            }
+        }
+        return res;
+    }
+}
+*/
