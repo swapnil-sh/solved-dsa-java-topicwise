084700a01c8bdaf5b8578584d61abd7ffee831f2
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-04-26 20:46:43 +0530

    516. Longest Palindromic Subsequence - Leetcode Java Solution

--- DynamicProgramming/LongCommPalinSubseq.java
+++ DynamicProgramming/LongCommPalinSubseq.java
@@ -62,3 +62,33 @@ class LongCommPalinSubseq
         
     }
 }
+
+
+/*
+----------------LCS Memoization-----------------
+TC - O(n^2)
+SC - O(n^2)
+*/
+
+/*
+int dp[100][100]={0};
+//Recursion with memoization (top_down) with two strings
+int longest(string s1,string s2,int i,int j){
+	if(i==s1.length() or j==s2.length()){
+		return 0;
+	}
+	if(dp[i][j]!=0){
+		return dp[i][j];
+	}
+	if(s1[i]==s2[j]){
+		return dp[i][j]=1+longest(s1,s2,i+1,j+1);
+	}
+	return dp[i][j]=max(longest(s1,s2,i+1,j),longest(s1,s2,i,j+1));
+}
+int longestPalindromeSubseq(string s){
+	string s1;
+	s1=s;
+	reverse(s1.begin(),s1.end());
+	return longest(s,s1,0,0);
+}
+*/
