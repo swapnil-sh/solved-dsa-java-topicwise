f1ccb91a105fdb209141db75633c70be11cefdfe
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-07-07 22:32:39 +0530

    Added Heap Solution

--- BinarySearch/KSmallInMatrix.java
+++ BinarySearch/KSmallInMatrix.java
@@ -52,3 +52,31 @@ class KSmallInMatrix
         
     }
 }
+
+
+/*
+-------------------Using Heap------------------------------
+TC - O(n^2logK)
+SC - O(n^2)
+*/
+/*
+class KSmallEleSortedMatrix{
+
+
+  // Solution One TC : O(nlogk)
+  public int kthSmallest(int[][] matrix, int k) {
+    PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a, b) -> (b - a));
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        pq.offer(matrix[i][j]);
+        if (pq.size() > k) {
+          pq.poll();
+        }
+
+      }
+    }
+
+    return pq.peek();
+  }
+*/
