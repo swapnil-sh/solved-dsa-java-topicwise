da2b840465fb0f048515f5a6ed0ed585ed54175f
swapnil-sh <swapnil.sharma2709@gmail.com>
2021-07-29 21:21:11 +0530

    1346. Check If N and Its Double Exist -- Leetcode Java Solutions--Added Binary Search Solution

--- HashSet/CheckNDoubleExists.java
+++ HashSet/CheckNDoubleExists.java
@@ -1,4 +1,4 @@
-/*1346. Check If N and Its Double Exist -- Leetcode Java Solutions*/
+/*1346. Check If N and Its Double Exist - Leetcode Java Solutions*/
 
 /*
 -----------------------HashSet Solution-----------------------------
@@ -24,3 +24,54 @@ class CheckNDoubleExists{
         return false;
     }
 }
+
+
+/*
+---------------------Binary Search Solution------------------------------
+
+TC - O(nlogn)
+SC - O(1)
+*/
+/*
+class Solution {
+    
+    public int binarySearch(int a[], int x){
+        
+        int st=0;
+        int end=a.length-1;
+        
+        while(st<=end){
+            
+            int mid=st+(end-st)/2;
+            if(a[mid]==x)
+                return mid;
+            else if(x>a[mid])
+                st=mid+1;
+            else
+                end=mid-1;
+        }
+        
+        return -1;
+    }
+    public boolean checkIfExist(int[] a) {
+        
+        
+        Arrays.sort(a);
+        
+        int i,zeroCount=0;
+        for(i=0;i<a.length;i++){
+            
+            if(a[i]!=0){
+                
+                int index=binarySearch(a, a[i]*2);
+                if(index!=-1 && index!=i)
+                    return true;
+            }
+            else
+                zeroCount++;
+        }
+        
+        return zeroCount>=2;
+    }
+}
+*/
