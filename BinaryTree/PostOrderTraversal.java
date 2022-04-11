589089b8b093d251ce1dd64fa4778e2966cf8c94
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-04-11 12:03:43 +0530

    Revised PostOrder Traversal and added new solution

--- BinaryTree/PostOrderTraversal.java
+++ BinaryTree/PostOrderTraversal.java
@@ -1,4 +1,6 @@
-/*145. Binary Tree Postorder Traversal - Leetcode Iterarive and Recursive Java Solution*/
+/*145. Binary Tree Postorder Traversal - Leetcode Iterarive and Recursive Java Solution
+https://leetcode.com/problems/binary-tree-postorder-traversal/
+*/
 
 /**
  * Definition for a binary tree node.
@@ -16,16 +18,59 @@
  * }
  */
 
+/*----------------Iterative Using One Stack-------------------------*/
+
+class PostOrderTraversal{
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+        List<Integer> res = new ArrayList<>();
+        if(root == null)
+            return res;
+        
+        Stack<TreeNode> st = new Stack<>();
+        TreeNode currNode = root;
+        TreeNode temp;
+        
+        while(currNode != null || !st.isEmpty()){
+            
+            if(currNode != null) {
+                st.push(currNode);
+                currNode = currNode.left;
+            }
+            else {
+                temp = st.peek().right;
+                if(temp == null) {
+                    temp = st.pop();
+                    res.add(temp.val);
+                    
+                    while(!st.isEmpty() && temp == st.peek().right) {
+                        temp = st.pop();
+                        res.add(temp.val);
+                    }
+                }
+                else 
+                    currNode = temp;
+            }
+        }
+        
+        return res;
+    }
+}
+
+
+/*----------------Iterative Using Two Stacks-------------------------*/
+
+/*
 class PostOrderTraversal
 {
     public List<Integer> postorderTraversal(TreeNode root)
     {
         List<Integer> res = new LinkedList<Integer>();
         
-		if(root==null) 
+	if(root==null) 
             return res;
         
-		Stack<TreeNode> s1=new Stack<>();
+	Stack<TreeNode> s1=new Stack<>();
         Stack<TreeNode> s2=new Stack<>();
         
         s1.push(root);
@@ -50,7 +95,7 @@ class PostOrderTraversal
         
     }
 }
-
+*/
 
 
 //--------------------Recursive Solution---------------------------
