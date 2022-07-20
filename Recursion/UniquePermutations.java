680174a6cd9371acc1bc25e5ba650c6985e791bb
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-07-21 02:24:00 +0530

    Unique Permutations of Sring/Array Java Solutions

--- Recursion/UniquePermutations.java
+++ Recursion/UniquePermutations.java
@@ -1,4 +1,5 @@
-/* Unique Permutations - CodeStudio Java Solutions
+/* Unique Permutations - Leetcode and CodeStudio Java Solutions
+https://leetcode.com/problems/permutations-ii/
 https://www.codingninjas.com/codestudio/problems/all-unique-permutations_1094902
 */
 
@@ -12,6 +13,42 @@ SC - O(n)+O(n)
 */
 
 
+class Solution {
+    
+    private void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    
+    private void util(int a[], int pos, List<List<Integer>> res) {
+        if(pos >= a.length) {
+            res.add(Arrays.stream(a).boxed().collect(Collectors.toList()));
+            return;
+        }
+        
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=pos; i<a.length; i++) {
+            if(!hs.contains(a[i])) {
+                hs.add(a[i]);
+                swap(a, i, pos);
+                util(a, pos+1, res);
+                swap(a, i, pos);
+             }
+        }
+    }
+    
+    public List<List<Integer>> permuteUnique(int[] a) {
+        List<List<Integer>> res = new ArrayList<>();
+        util(a, 0, res);
+        return res;
+    }
+}
+
+
+/*
+Same as above solution but solved on Codestudio
+*/
 import java.util.*;
 
 public class UniquePermutations {
