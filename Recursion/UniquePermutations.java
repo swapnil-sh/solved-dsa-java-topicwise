1d9c55b69072c54271b434eb06994e4144d2c499
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-07-21 02:09:08 +0530

    Unique Permutations of Sring/Array Java Solutions

---
+++ Recursion/UniquePermutations.java
@@ -0,0 +1,42 @@
+/* Unique Permutations - CodeStudio Java Solutions
+https://www.codingninjas.com/codestudio/problems/all-unique-permutations_1094902?source=youtube&campaign=Recursion_Fraz&utm_source=youtube&utm_medium=affiliate&utm_campaign=Recursion_Fraz&leftPanelTab=1
+*/
+
+
+/*
+----------------- Recursive Approach using Swappping and Hashet--------------------
+
+TC - O(n*n!)
+SC - O(n)+O(n)
+
+*/
+
+
+import java.util.*;
+
+public class UniquePermutations {
+   
+    private static void util(ArrayList<Integer> a, int pos, ArrayList<ArrayList<Integer>> res) {
+        if(pos >= a.size()) {
+            res.add(new ArrayList<>(a));
+            return;
+        }
+        
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=pos; i<a.size(); i++) {
+            if(!hs.contains(a.get(i))) {
+                hs.add(a.get(i));
+                Collections.swap(a, i, pos);
+                util(a, pos+1, res);
+                Collections.swap(a, i, pos);
+             }
+        }
+    }
+    public static ArrayList<ArrayList< Integer >> uniquePermutations(ArrayList<Integer> a, int n) {
+        // Write your code here.
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        util(a, 0, res);
+        return res;
+    }
+}
+
