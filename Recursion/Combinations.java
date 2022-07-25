9e2dd561b685b195b4857baf397b04e19ad01344
swapnil-sh <swapnil.sharma2709@gmail.com>
2022-07-25 22:36:41 +0530

    Combinations - Java Solutions

---
+++ Recursion/Combinations.java
@@ -0,0 +1,35 @@
+/*77. Combinations - Leetcode Java Solution
+https://leetcode.com/problems/combinations/
+*/
+
+
+/*
+-------------Recursion using Choice Algorithm-------------------------
+TC - O(2^n)
+SC - (n) (height of tree)
+*/
+
+class Combinations {
+    
+    private void util(int n, int k, int i, List<Integer> temp, List<List<Integer>> res) {
+        
+        if(i>n) {
+            if(temp.size()==k)
+                res.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        temp.add(i);
+        util(n, k, i+1, temp, res);
+        temp.remove(temp.size()-1);
+        util(n, k, i+1, temp, res);
+        return;
+    }
+    
+    public List<List<Integer>> combine(int n, int k) {
+        
+        List<List<Integer>> res = new ArrayList<>();
+        util(n, k, 1, new ArrayList<>(), res);
+        return res;
+    }
+}
