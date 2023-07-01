25aadb2f22b4708093658e137b3a22de90c3af20
swapnil-sh <swapnil.sharma2709@gmail.com>
2023-07-01 20:00:04 +0530

    added SubsetII leetcode solution

---
+++ Recursion/SubsetII.java
@@ -0,0 +1,80 @@
+/*
+
+Subset II - Leetcode
+https://leetcode.com/problems/subsets-ii/
+
+Backtracking
+We need to know when we should not add a list to the result list.
+By observation, a duplicate list occurs when i >= 1 (when i = 0, duplicate cannot occur) and nums[i - 1] == nums[i] and in the previous step we did not pick nums[i - 1]. The information of whether it picks or not could be passed down by a boolean parameter isPicked.
+
+If the above condition is satisfied:
+
+Do not add the list to the result list.
+Do not do the subproblem after picking the current element.
+Only do the subproblem after not picking the current element.
+Note: Be careful where we should put the numList.add(val) and nu
+
+Time: O(N * 2^N)
+Space: O(N * 2^N)
+*/
+
+class SubsetII {
+    private void util(int[] a, int i, List<Integer> temp, List<List<Integer>> res) {
+        if (i >= a.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        temp.add(a[i]);
+        util(a, i+1, temp, res);
+
+        temp.remove(temp.size() - 1);
+
+        while(i<a.length-1 && a[i] == a[i+1]) {
+            i++;
+        }
+        
+        util(a, i+1, temp, res);
+    }
+    public List<List<Integer>> subsetsWithDup(int[] a) {
+        Arrays.sort(a);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        util(a, 0, temp, res);
+        return res;
+    }
+}
+
+
+class SubsetII {
+    private void subsets(int[] nums,int i, List<Integer> numList, List<List<Integer>> result, boolean isPicked) {
+        // base case
+        if (i >= nums.length) {
+            return;
+        }
+        int val = nums[i];
+        // not pick
+        subsets(i + 1, nums, numList, result, false);
+        // duplicate check
+        if (i >= 1 && nums[i - 1] == nums[i] && isPicked == false) {
+            return;
+        }
+        // pick
+        numList.add(val);
+        subsets(i + 1, nums, numList, result, true);
+        result.add(new ArrayList<>(numList));  // add to the result list
+        numList.remove(numList.size() - 1); 
+    }
+    public List<List<Integer>> subsetsWithDup(int[] a) {
+        Arrays.sort(a);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        util(a, 0, temp, res, true);
+        return res;
+    }
+}
+
+
+
